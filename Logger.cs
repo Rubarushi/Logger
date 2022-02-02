9ad87dbdd05524902b32d3a3ca49d68fb8350e55
Rubarushi <89915932+Rubarushi@users.noreply.github.com>
2022-02-02 14:17:41 +0900

    Add files via upload

---
+++ Logger.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+
+	internal class ConsoleContents
+	{
+		public string Message = string.Empty;
+		public Type mType = null;
+		public ConsoleColor ForeGroundColor = default;
+		public ConsoleColor BackGroundColor = default;
+		public bool WriteLine = true;
+	}
+
+	public class Logger
+	{
+		public static void StartLogger()
+		{
+			Task.Factory.StartNew(() =>
+			{
+				foreach (ConsoleContents CC in Collections.GetConsumingEnumerable())
+				{
+					if (Console.BackgroundColor != CC.BackGroundColor)
+					{
+						Console.BackgroundColor = CC.BackGroundColor;
+					}
+
+					if (Console.ForegroundColor != CC.ForeGroundColor)
+					{
+						Console.ForegroundColor = CC.ForeGroundColor;
+					}
+
+					if (CC.WriteLine)
+					{
+						CC.Message += "\r\n";
+					}
+
+					Console.Write(CC.Message);
+				}
+			});
+		}
+
+		private ConsoleColor BackColor = default;
+		private ConsoleColor ForeColor = default;
+		private Type type;
+
+		public Logger(Type type, ConsoleColor BackColor = default, ConsoleColor ForeColor = default)
+        {
+            this.BackColor = BackColor;
+            this.ForeColor = ForeColor;
+			this.type = type;
+
+			LoggerDic.TryAdd(type, this);
+		}
+
+		public static void RemoteLogger(Type type)
+        {
+			LoggerDic.TryRemove(type, out _);
+        }
+
+		public static Logger GetLogger(Type type)
+        {
+			return LoggerDic[type];
+        }
+
+		private static BlockingCollection<ConsoleContents> Collections = new BlockingCollection<ConsoleContents>();
+		private static ConcurrentDictionary<Type, Logger> LoggerDic = new ConcurrentDictionary<Type, Logger>();
+
+		public void WriteLine(string msg, params object[] args)
+		{
+			WriteLine(string.Format(msg, args));
+		}
+
+		public void WriteLine(string msg)
+		{
+            ConsoleContents CC = new ConsoleContents
+            {
+                Message = msg,
+                mType = type,
+                WriteLine = true,
+                ForeGroundColor = ForeColor,
+                BackGroundColor = BackColor
+            };
+
+            Collections.Add(CC);
+		}
+
+		public void Write(string msg, params object[] args)
+		{
+			Write(string.Format(msg, args));
+		}
+
+		public void Write(string msg)
+		{
+            ConsoleContents CC = new ConsoleContents
+            {
+                Message = msg,
+                mType = type,
+                WriteLine = false,
+                ForeGroundColor = ForeColor,
+                BackGroundColor = BackColor
+            };
+            Collections.Add(CC);
+		}
+	}
+}
